84e1df376a9c8103cfd5a0dcbc1ba6bed69c7c7a
GrupoFatecZL <grupodafateczl@gmail.com>
2022-03-21 16:47:52 -0300

    publish repository init project

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDCDKa_oSI7iAW1-eDBmk0z68XVrCMfHY",
+  authDomain: "doa-acao-homolog.firebaseapp.com",
+  projectId: "doa-acao-homolog",
+  storageBucket: "doa-acao-homolog.appspot.com",
+  messagingSenderId: "978293478291",
+  appId: "1:978293478291:web:27fba032d6720a48898e66",
+  measurementId: "G-V0K1WGKGGB"
+};
+
+const app = initializeApp(firebaseConfig);
